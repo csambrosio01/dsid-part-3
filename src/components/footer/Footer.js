b08cbf6b450141b1cb3ccca965a679f3d2928602
Caio Ambrosio <ambrosio.caio13@gmail.com>
2020-06-23 10:29:19 -0300

    Create basic component named Footer

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './Footer.css'
+
+const Footer = () => (
+    <footer className="app-footer">
+        <span className="app-footer-message">
+            Footer
+        </span>
+    </footer>
+)
+
+export default Footer
