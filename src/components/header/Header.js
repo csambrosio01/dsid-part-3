02d8baaf73c05360505045dace4be062e7815e47
Caio Ambrosio <ambrosio.caio13@gmail.com>
2020-06-23 16:26:36 -0300

    Use bootswatch to modify header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -7,12 +7,24 @@ import Menu from '../menu'
 import './Header.css'
 
 const Header = () => (
-    <header className="app-header">
-        <span>
-            <img src={logo} className="app-header-logo" alt="Imagem do Logo da empresa Pousar.com"/>
-        </span>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a className="navbar-brand" href="/">
+            <span>
+                <img src={logo} className="app-header-logo" alt="Imagem do Logo da empresa Pousar.com"/>
+            </span>
+        </a>
+        <button className="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarColor02"
+                aria-controls="navbarColor02"
+                aria-expanded="false"
+                aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"/>
+        </button>
+
         <Menu/>
-    </header>
+    </nav>
 )
 
 export default Header
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 
 import Main from "./components/Main";
 
+import 'bootswatch/dist/cerulean/bootstrap.min.css'
 import './index.css'
 
 ReactDOM.render(<Main/>, document.getElementById('root'))
