dd400ff4ff1bfa29295b0c121674f90044447815
Caio Ambrosio <ambrosio.caio13@gmail.com>
2020-06-24 13:57:02 -0300

    Add basic form for login

--- src/contents/user/Login.js
+++ src/contents/user/Login.js
@@ -1,11 +1,40 @@
 import React from "react";
 import App from "../../containers/App";
+import Card from "../../components/Card";
 
 class Login extends React.Component {
     render() {
         return (
             <App>
-                <h1>Login</h1>
+                <Card header={"Logue em sua conta"}>
+                    <form>
+                        <div className="row">
+                            <div className="col-md-6">
+                                <div className="form-group">
+                                    <label>Usuário:</label>
+                                    <input type="text"
+                                           name="username"
+                                           className="form-control"/>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-md-6">
+                                <div className="form-group">
+                                    <label>Senha:</label>
+                                    <input type="password"
+                                           name="password"
+                                           className="form-control"/>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-md-1">
+                                <button type="submit" className="btn btn-primary">Logar</button>
+                            </div>
+                        </div>
+                    </form>
+                </Card>
             </App>
         )
     }
