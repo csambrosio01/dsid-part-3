64a780e1fccfc949b106403758edf568ad34c31e
Caio Ambrosio <ambrosio.caio13@gmail.com>
2020-06-24 17:00:20 -0300

    Fix menu items visualization in mobile

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -4,31 +4,31 @@ import {Link} from 'react-router-dom'
 const Menu = () => (
     <div className="collapse navbar-collapse" id="navbarColor02">
         <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
+            <li className="nav-item active d-inline-block text-center">
                 <Link className="nav-link" to="/air">
                     Aéreo
                 </Link>
             </li>
 
-            <li className="nav-item active">
+            <li className="nav-item active d-inline-block text-center">
                 <Link className="nav-link" to="/hotel">
                     Hotéis
                 </Link>
             </li>
 
-            <li className="nav-item active">
+            <li className="nav-item active d-inline-block text-center">
                 <Link className="nav-link" to="/car-rental">
                     Aluguel de carros
                 </Link>
             </li>
 
-            <li className="nav-item active">
+            <li className="nav-item active d-inline-block text-center">
                 <Link className="nav-link" to="/login">
                     Login
                 </Link>
             </li>
 
-            <li className="nav-item active">
+            <li className="nav-item active d-inline-block text-center">
                 <Link className="nav-link" to="/create-user">
                     Cadastre-se
                 </Link>
