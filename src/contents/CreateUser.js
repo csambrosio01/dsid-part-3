caed6d675d56cb00df4724d15270ea13dacb406f
Caio Ambrosio <ambrosio.caio13@gmail.com>
2020-06-24 12:53:54 -0300

    Extract card to a component

--- src/contents/CreateUser.js
+++ src/contents/CreateUser.js
@@ -6,6 +6,7 @@ import NumberFormat from 'react-number-format';
 import UserService from '../app/UserService'
 
 import App from "../containers/App";
+import Card from "../components/Card";
 
 const initialState = {
     user: {
@@ -108,105 +109,100 @@ class CreateUser extends React.Component {
     render() {
         return (
             <App>
-                <div className="card">
-                    <div className="card-header">
-                        Cadastrar um novo usuário
-                    </div>
-                    <div className="card-body">
-                        <form onSubmit={this.onSubmit}>
-                            <div className="row">
-                                <div className="col-md-6">
-                                    <div className="form-group">
-                                        <label>Nome Completo:</label>
-                                        <input type="text"
-                                               name="name"
-                                               value={this.state.user.name}
-                                               onChange={this.onChange}
-                                               onBlur={this.onBlur}
-                                               className="form-control"/>
-                                        <span style={{color: "red"}}>{this.state.errors["name"]}</span>
-                                    </div>
+                <Card header={"Cadastrar um novo usuário"}>
+                    <form onSubmit={this.onSubmit}>
+                        <div className="row">
+                            <div className="col-md-6">
+                                <div className="form-group">
+                                    <label>Nome Completo:</label>
+                                    <input type="text"
+                                           name="name"
+                                           value={this.state.user.name}
+                                           onChange={this.onChange}
+                                           onBlur={this.onBlur}
+                                           className="form-control"/>
+                                    <span style={{color: "red"}}>{this.state.errors["name"]}</span>
                                 </div>
                             </div>
-                            <div className="row">
-                                <div className="col-md-6">
-                                    <div className="form-group">
-                                        <label>E-mail:</label>
-                                        <input type="text"
-                                               name="email"
-                                               value={this.state.user.email}
-                                               onChange={this.onChange}
-                                               onBlur={this.onBlur}
-                                               className="form-control"/>
-                                        <span style={{color: "red"}}>{this.state.errors["email"]}</span>
-                                    </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-md-6">
+                                <div className="form-group">
+                                    <label>E-mail:</label>
+                                    <input type="text"
+                                           name="email"
+                                           value={this.state.user.email}
+                                           onChange={this.onChange}
+                                           onBlur={this.onBlur}
+                                           className="form-control"/>
+                                    <span style={{color: "red"}}>{this.state.errors["email"]}</span>
                                 </div>
                             </div>
-                            <div className="row">
-                                <div className="col-md-6">
-                                    <div className="form-group">
-                                        <label>Telefone:</label>
-                                        <NumberFormat name="phoneNumber"
-                                                      format="(##) ##### ####"
-                                                      value={this.state.user.phoneNumber}
-                                                      onChange={this.onChange}
-                                                      onBlur={this.onBlur}
-                                                      className="form-control"/>
-                                        <span style={{color: "red"}}>{this.state.errors["phoneNumber"]}</span>
-                                    </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-md-6">
+                                <div className="form-group">
+                                    <label>Telefone:</label>
+                                    <NumberFormat name="phoneNumber"
+                                                  format="(##) ##### ####"
+                                                  value={this.state.user.phoneNumber}
+                                                  onChange={this.onChange}
+                                                  onBlur={this.onBlur}
+                                                  className="form-control"/>
+                                    <span style={{color: "red"}}>{this.state.errors["phoneNumber"]}</span>
                                 </div>
                             </div>
-                            <div className="row">
-                                <div className="col-md-6">
-                                    <div className="form-group">
-                                        <label>Usuário:</label>
-                                        <input type="text"
-                                               name="username"
-                                               value={this.state.user.username}
-                                               onChange={this.onChange}
-                                               onBlur={this.onBlur}
-                                               className="form-control"/>
-                                        <span style={{color: "red"}}>{this.state.errors["username"]}</span>
-                                    </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-md-6">
+                                <div className="form-group">
+                                    <label>Usuário:</label>
+                                    <input type="text"
+                                           name="username"
+                                           value={this.state.user.username}
+                                           onChange={this.onChange}
+                                           onBlur={this.onBlur}
+                                           className="form-control"/>
+                                    <span style={{color: "red"}}>{this.state.errors["username"]}</span>
                                 </div>
                             </div>
-                            <div className="row">
-                                <div className="col-md-6">
-                                    <div className="form-group">
-                                        <label>Senha:</label>
-                                        <input type="password"
-                                               name="password"
-                                               value={this.state.user.password}
-                                               onChange={this.onChange}
-                                               onBlur={this.onBlur}
-                                               className="form-control"/>
-                                        <span style={{color: "red"}}>{this.state.errors["password"]}</span>
-                                    </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-md-6">
+                                <div className="form-group">
+                                    <label>Senha:</label>
+                                    <input type="password"
+                                           name="password"
+                                           value={this.state.user.password}
+                                           onChange={this.onChange}
+                                           onBlur={this.onBlur}
+                                           className="form-control"/>
+                                    <span style={{color: "red"}}>{this.state.errors["password"]}</span>
                                 </div>
                             </div>
-                            <div className="row">
-                                <div className="col-md-6">
-                                    <div className="form-group">
-                                        <label>Confirmar senha:</label>
-                                        <input type="password"
-                                               name="confirmPassword"
-                                               value={this.state.user.confirmPassword}
-                                               onChange={this.onChange}
-                                               onBlur={this.onBlur}
-                                               className="form-control"/>
-                                        <span style={{color: "red"}}>{this.state.errors["confirmPassword"]}</span>
-                                    </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-md-6">
+                                <div className="form-group">
+                                    <label>Confirmar senha:</label>
+                                    <input type="password"
+                                           name="confirmPassword"
+                                           value={this.state.user.confirmPassword}
+                                           onChange={this.onChange}
+                                           onBlur={this.onBlur}
+                                           className="form-control"/>
+                                    <span style={{color: "red"}}>{this.state.errors["confirmPassword"]}</span>
                                 </div>
                             </div>
+                        </div>
 
-                            <div className="row">
-                                <div className="col-md-1">
-                                    <button type="submit" className="btn btn-primary">Cadastrar</button>
-                                </div>
+                        <div className="row">
+                            <div className="col-md-1">
+                                <button type="submit" className="btn btn-primary">Cadastrar</button>
                             </div>
-                        </form>
-                    </div>
-                </div>
+                        </div>
+                    </form>
+                </Card>
             </App>
         )
     }
