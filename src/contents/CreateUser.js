08b481a7f0117944bb4e24f078b9a5404b81c1f7
Caio Ambrosio <ambrosio.caio13@gmail.com>
2020-06-24 11:43:14 -0300

    Add form to create an user

--- src/contents/CreateUser.js
+++ src/contents/CreateUser.js
@@ -6,7 +6,81 @@ class CreateUser extends React.Component {
     render() {
         return (
             <App>
-                <h1>Create User</h1>
+                <div className="card">
+                    <div className="card-header">
+                        Cadastrar um novo usuário
+                    </div>
+                    <div className="card-body">
+                        <form>
+                            <div className="row">
+                                <div className="col-md-6">
+                                    <div className="form-group">
+                                        <label>Nome Completo:</label>
+                                        <input type="text"
+                                               name="name"
+                                               className="form-control"/>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-md-6">
+                                    <div className="form-group">
+                                        <label>E-mail:</label>
+                                        <input type="text"
+                                               name="email"
+                                               className="form-control"/>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-md-6">
+                                    <div className="form-group">
+                                        <label>Telefone:</label>
+                                        <input type="text"
+                                               name="phoneNumber"
+                                               className="form-control"/>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-md-6">
+                                    <div className="form-group">
+                                        <label>Usuário:</label>
+                                        <input type="text"
+                                               name="username"
+                                               className="form-control"/>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-md-6">
+                                    <div className="form-group">
+                                        <label>Senha:</label>
+                                        <input type="text"
+                                               name="password"
+                                               className="form-control"/>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-md-6">
+                                    <div className="form-group">
+                                        <label>Confirmar senha:</label>
+                                        <input type="password"
+                                               name="confirmPassword"
+                                               className="form-control"/>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="row">
+                                <div className="col-md-1">
+                                    <button type="submit" className="btn btn-primary">Cadastrar</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </App>
         )
     }
