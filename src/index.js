b59678d5944ca9e3d2016dbca14916f93b0ca24c
Caio Ambrosio <ambrosio.caio13@gmail.com>
2020-06-24 15:57:59 -0300

    Add react-load-spinner css to index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 
 import Main from "./components/Main";
 
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
 import 'react-notifications-component/dist/theme.css';
 import 'animate.css';
 import 'bootswatch/dist/cerulean/bootstrap.min.css'
