c3d0b76e3fdd07cc430bd2dc3011b21d251517e0
Caio Ambrosio <ambrosio.caio13@gmail.com>
2020-06-24 17:17:58 -0300

    Add call to getUser in componentDidMount of Menu Component
    If there is a logged user, hide login and sign in pages

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,42 +1,67 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import UserService from "../../app/UserService";
 
-const Menu = () => (
-    <div className="collapse navbar-collapse" id="navbarColor02">
-        <ul className="navbar-nav mr-auto">
-            <li className="nav-item active d-inline-block text-center">
-                <Link className="nav-link" to="/air">
-                    Aéreo
-                </Link>
-            </li>
+class Menu extends React.Component {
+    state = {
+        user: undefined
+    }
 
-            <li className="nav-item active d-inline-block text-center">
-                <Link className="nav-link" to="/hotel">
-                    Hotéis
-                </Link>
-            </li>
+    constructor(props) {
+        super(props);
+        this.userService = new UserService();
+    }
 
-            <li className="nav-item active d-inline-block text-center">
-                <Link className="nav-link" to="/car-rental">
-                    Aluguel de carros
-                </Link>
-            </li>
-        </ul>
+    componentDidMount() {
+        this.userService.getUser()
+            .then(user => {
+                this.setState({user})
+            })
+    }
 
-        <ul className="navbar-nav my-2 my-lg-0">
-            <li className="nav-item active d-inline-block text-center">
-                <Link className="nav-link" to="/login">
-                    Login
-                </Link>
-            </li>
+    render() {
+        return (
+            <div className="collapse navbar-collapse" id="navbarColor02">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item active d-inline-block text-center">
+                        <Link className="nav-link" to="/air">
+                            Aéreo
+                        </Link>
+                    </li>
 
-            <li className="nav-item active d-inline-block text-center">
-                <Link className="nav-link" to="/create-user">
-                    Cadastre-se
-                </Link>
-            </li>
-        </ul>
-    </div>
-)
+                    <li className="nav-item active d-inline-block text-center">
+                        <Link className="nav-link" to="/hotel">
+                            Hotéis
+                        </Link>
+                    </li>
+
+                    <li className="nav-item active d-inline-block text-center">
+                        <Link className="nav-link" to="/car-rental">
+                            Aluguel de carros
+                        </Link>
+                    </li>
+                </ul>
+
+                <ul className="navbar-nav my-2 my-lg-0">
+                    {!this.state.user &&
+                    <li className="nav-item active d-inline-block text-center">
+                        <Link className="nav-link" to="/login">
+                            Login
+                        </Link>
+                    </li>
+                    }
+
+                    {!this.state.user &&
+                    <li className="nav-item active d-inline-block text-center">
+                        <Link className="nav-link" to="/create-user">
+                            Cadastre-se
+                        </Link>
+                    </li>
+                    }
+                </ul>
+            </div>
+        )
+    }
+}
 
 export default Menu
