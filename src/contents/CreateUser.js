3dd9ecf248c3881c0d82f36949ac1079ab8ff45c
Caio Ambrosio <ambrosio.caio13@gmail.com>
2020-06-24 11:39:45 -0300

    Add view CreateUser

---
+++ src/contents/CreateUser.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import App from "../containers/App";
+
+class CreateUser extends React.Component {
+    render() {
+        return (
+            <App>
+                <h1>Create User</h1>
+            </App>
+        )
+    }
+}
+
+export default CreateUser;
