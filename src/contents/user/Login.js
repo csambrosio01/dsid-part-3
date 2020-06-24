329b7236ce0798fe80130397fa01dc4a0e8d4be4
Caio Ambrosio <ambrosio.caio13@gmail.com>
2020-06-24 13:00:48 -0300

    Add App and container to login page

--- src/contents/user/Login.js
+++ src/contents/user/Login.js
@@ -1,9 +1,14 @@
 import React from "react";
+import App from "../../containers/App";
 
 class Login extends React.Component {
     render() {
         return (
-            <h1>Login</h1>
+            <App>
+                <div className="container">
+                    <h1>Login</h1>
+                </div>
+            </App>
         )
     }
 }
