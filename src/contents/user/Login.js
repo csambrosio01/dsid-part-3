aafc9a008919597b14bf1b9c33f87eed973cd306
Caio Ambrosio <ambrosio.caio13@gmail.com>
2020-06-24 15:25:11 -0300

    Add onSubmit to submit login form

--- src/contents/user/Login.js
+++ src/contents/user/Login.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {store} from 'react-notifications-component';
 import App from "../../containers/App";
 import Card from "../../components/Card";
 import UserService from "../../app/UserService";
@@ -35,11 +36,61 @@ class Login extends React.Component {
         this.setState({errors})
     }
 
+    validateForm = (login) => {
+        let errors = this.userService.validate(login)
+
+        let valid = true;
+        Object.values(errors).forEach(
+            (val) => val.length > 0 && (valid = false)
+        );
+
+        this.setState({errors})
+        return valid;
+    }
+
+    onSubmit = (event) => {
+        event.preventDefault();
+
+        let login = this.state.login;
+        if (this.validateForm(login)) {
+            debugger;
+            this.userService.login(login)
+                .then (() => {
+                    store.addNotification({
+                        title: 'Sucesso!',
+                        message: 'Você foi logado com sucesso',
+                        type: 'success',
+                        container: 'top-center',
+                        animationIn: ["animated", "fadeIn"],
+                        animationOut: ["animated", "fadeOut"],
+                        dismiss: {
+                            duration: 3000
+                        }
+                    })
+
+                    this.props.history.push('/')
+                })
+                .catch(error => {
+                    store.addNotification({
+                        title: 'Falha!',
+                        message: error.response.data.error,
+                        type: 'danger',
+                        container: 'top-center',
+                        animationIn: ["animated", "fadeIn"],
+                        animationOut: ["animated", "fadeOut"],
+                        dismiss: {
+                            duration: 3000
+                        }
+                    })
+                })
+        }
+    }
+
     render() {
         return (
             <App>
                 <Card header={"Logue em sua conta"}>
-                    <form>
+                    <form onSubmit={this.onSubmit}>
                         <div className="row">
                             <div className="col-md-6">
                                 <div className="form-group">
