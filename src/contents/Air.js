b5fbf24efd9b1d28b683bcad4b590a99fb9aefef
Caio Ambrosio <ambrosio.caio13@gmail.com>
2020-07-01 12:02:45 -0300

    Add highlights to air page

--- src/contents/Air.js
+++ src/contents/Air.js
@@ -2,8 +2,39 @@ import React from 'react'
 
 import App from "../containers/App";
 import FlightSearchCard from "../components/air/flightSearchCard/FlightSearchCard";
+import FlightService from "../app/FlightService";
+import Loader from "react-loader-spinner";
+import FlightInfoCard from "../components/air/flightInfoCard/FlightInfoCard";
 
 class Air extends React.Component {
+    state = {
+        flightOffers: [],
+        isFlightOffersLoading: false
+    }
+
+    constructor(props) {
+        super(props);
+        this.flightService = new FlightService();
+    }
+
+    componentDidMount() {
+        this.getFlightOffersHighlights()
+    }
+
+    getFlightOffersHighlights = () => {
+        this.setState({isFlightOffersLoading: true})
+        this.flightService.getFlightOffersHighlightsAirPage()
+            .then(response => {
+                this.setState({
+                    flightOffers: response.data,
+                    isFlightOffersLoading: false
+                })
+            })
+            .catch(() => {
+                this.setState({isFlightOffersLoading: false})
+            })
+    }
+
     render() {
         return (
             <App>
@@ -16,6 +47,28 @@ class Air extends React.Component {
 
                     <FlightSearchCard/>
                 </div>
+                {!this.state.isFlightOffersLoading &&
+                <div className="mb-5">
+                    <div className="row mb-3">
+                        <div className="col-md-12">
+                            <h2>Principais ofertas de passagens</h2>
+                        </div>
+                    </div>
+                    <div className="row">
+
+                        {this.state.flightOffers.map(flightOffer => {
+                            return (
+                                <FlightInfoCard flightOffer={flightOffer}/>
+                            )
+                        })}
+                    </div>
+                </div>
+                }
+                {this.state.isFlightOffersLoading &&
+                <div className="col-md-12 d-flex justify-content-center mb-5">
+                    <Loader type="Oval" color="Blue" height={100} width={100}/>
+                </div>
+                }
             </App>
         )
     }
