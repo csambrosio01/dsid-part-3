4db6ec67f3a20dbad5f9e7b1c60624861f3455da
Caio Ambrosio <ambrosio.caio13@gmail.com>
2020-07-01 13:58:09 -0300

    Add getFlightOrder method on flight service

--- src/app/FlightService.js
+++ src/app/FlightService.js
@@ -1,6 +1,25 @@
 import Api from "../api/Api";
 
 class FlightService {
+    getFlightOrders = (searchObject) => {
+
+        let flight = {
+            originLocationCode: searchObject.origin,
+            destinationLocationCode: searchObject.destination,
+            departureDate: searchObject.departureDate.toISOString().split('T')[0],
+            adults: searchObject.passenger,
+            travelClass: searchObject.travelClass,
+            oneWay: searchObject.oneWay,
+            max: 9
+        }
+
+        if (!searchObject.oneWay) {
+            flight.returnDate = searchObject.returnDate.toISOString().split('T')[0]
+        }
+
+        return Api.post('/flight-offers', flight)
+    }
+
     getFlightOffersHighlights = () => {
         return Api.get('/flight-offers/highlights')
     }
