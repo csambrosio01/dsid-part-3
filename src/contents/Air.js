d985605de92a919fbfff4e9e92649fa6d8f5cb9a
Caio Ambrosio <ambrosio.caio13@gmail.com>
2020-07-01 11:07:25 -0300

    Add FlightCard to Air page, also make some changes on view

--- src/contents/Air.js
+++ src/contents/Air.js
@@ -1,12 +1,21 @@
 import React from 'react'
 
 import App from "../containers/App";
+import FlightSearchCard from "../components/air/flightSearchCard/FlightSearchCard";
 
 class Air extends React.Component {
     render() {
         return (
             <App>
-                <h1>Air</h1>
+                <div className="jumbotron">
+                    <h1 className="display-3">Aéreo</h1>
+
+                    <p className="lead">Encontre aqui as melhores ofertas de passagens para a sua viagem.</p>
+
+                    <hr className="my-4"/>
+
+                    <FlightSearchCard/>
+                </div>
             </App>
         )
     }
