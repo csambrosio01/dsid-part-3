e5df52db6715aa28071ae5e747e639d7db3cbe5a
Caio Ambrosio <ambrosio.caio13@gmail.com>
2020-07-01 10:47:15 -0300

    Create new component to make search for city or airport in api

---
+++ src/components/air/flightSearchCard/SearchInput.js
@@ -0,0 +1,48 @@
+import React from "react";
+import AirportService from "../../../app/AirportService";
+
+class SearchInput extends React.Component {
+    state = {
+        options: []
+    }
+
+    constructor(props) {
+        super(props);
+        this.airportService = new AirportService();
+    }
+
+    onTextChange = (event) => {
+        const value = event.target.value
+        if (value.length >= 3) {
+            this.airportService.searchCityOrAirport(value)
+                .then(response => {
+                    this.setState({options: response.data})
+                })
+        } else {
+            this.setState({options: []})
+        }
+    }
+
+    render() {
+        return (
+            <div className="input-icons">
+                <i className={this.props.icon}/>
+                <input
+                    className="input-field"
+                    type="search"
+                    list={this.props.placeholder}
+                    onChange={this.onTextChange}
+                    placeholder={this.props.placeholder}/>
+                <datalist id={this.props.placeholder}>
+                    {this.state.options.map(option => {
+                        return (
+                            <option value={option.name} key={option.id}>{option.subType}</option>
+                        )
+                    })}
+                </datalist>
+            </div>
+        )
+    }
+}
+
+export default SearchInput;
