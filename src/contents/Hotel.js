99db516ac781ce7b5fd70a50838f07023ca05f77
Caio Ambrosio <ambrosio.caio13@gmail.com>
2020-07-02 10:46:32 -0300

    Refact hotel view into a class

--- src/contents/Hotel.js
+++ src/contents/Hotel.js
@@ -2,10 +2,14 @@ import React from 'react'
 
 import App from "../containers/App";
 
-const Hotel = () => (
-    <App>
-        <h1>Hotel</h1>
-    </App>
-)
+class Hotel extends React.Component {
+    render() {
+        return (
+            <App>
+                <h1>Hotel</h1>
+            </App>
+        )
+    }
+}
 
 export default Hotel
