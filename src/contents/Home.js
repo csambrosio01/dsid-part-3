010e04eb7315d37d97f8872bdbd76aae068646e8
Caio Ambrosio <ambrosio.caio13@gmail.com>
2020-07-01 21:35:18 -0300

    Add HotelHighlight to home view

--- src/contents/Home.js
+++ src/contents/Home.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 import App from "../containers/App";
 import FlightHighlight from "../components/home/flightHighlight/FlightHighlight";
+import HotelHighlight from "../components/home/hotelHighlight/HotelHighlight";
+import HotelService from "../app/HotelService";
 import FlightService from "../app/FlightService";
 import Loader from "react-loader-spinner";
 
@@ -10,15 +12,18 @@ class Home extends React.Component {
         flightOffers: [],
         hotelOffers: [],
         isFlightOffersHighlightLoading: false,
+        isHotelOffersHighlightsLoading: false
     }
 
     constructor(props) {
         super(props);
         this.flightService = new FlightService();
+        this.hotelService = new HotelService();
     }
 
     componentDidMount() {
         this.getFlightOffersHighlights()
+        this.getHotelOffersHighlights()
     }
 
     getFlightOffersHighlights = () => {
@@ -35,6 +40,20 @@ class Home extends React.Component {
             })
     }
 
+    getHotelOffersHighlights = () => {
+        this.setState({isHotelOffersHighlightsLoading: true})
+        this.hotelService.getHotelOffersHighlights()
+            .then(response => {
+                this.setState({
+                    hotelOffers: response.data,
+                    isHotelOffersHighlightsLoading: false
+                })
+            })
+            .catch(() => {
+                this.setState({isHotelOffersHighlightsLoading: false})
+            })
+    }
+
     render() {
         return (
             <App>
@@ -42,16 +61,18 @@ class Home extends React.Component {
                     <h1>Principais ofertas</h1>
                 </ol>
 
-                {!this.state.isFlightOffersHighlightLoading &&
+                {(!this.state.isFlightOffersHighlightLoading &&
+                  !this.state.isHotelOffersHighlightsLoading) &&
                 <React.Fragment>
                     <FlightHighlight flightOffers={this.state.flightOffers}/>
 
-                <h2>Hotéis</h2>
+                    <HotelHighlight hotelOffers={this.state.hotelOffers}/>
 
                     <h2>Carros</h2>
                 </React.Fragment>
                 }
-                {this.state.isFlightOffersHighlightLoading &&
+                {(this.state.isFlightOffersHighlightLoading ||
+                  this.state.isHotelOffersHighlightsLoading) &&
                 <div className="d-flex justify-content-center">
                     <Loader type="Oval" color="Blue" height={100} width={100}/>
                 </div>
