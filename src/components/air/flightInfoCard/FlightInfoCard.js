1b8522f99a62f86757d157bbcc09d830dcba0d4f
Caio Ambrosio <ambrosio.caio13@gmail.com>
2020-07-01 21:32:20 -0300

    Make header in flight info card use h5 tag

--- src/components/air/flightInfoCard/FlightInfoCard.js
+++ src/components/air/flightInfoCard/FlightInfoCard.js
@@ -13,7 +13,9 @@ class FlightInfoCard extends React.Component {
             <div className="col-md-4">
                 <div className="card text-white bg-dark mb-3">
                     <div className="card-header">
-                        Viaje de {this.props.flightOffer.itineraries[0].segments[0].departure.iataCode} para {this.props.flightOffer.itineraries[0].segments[0].arrival.iataCode}
+                        <h5 className="card-text">
+                            Viaje de {this.props.flightOffer.itineraries[0].segments[0].departure.iataCode} para {this.props.flightOffer.itineraries[0].segments[0].arrival.iataCode}
+                        </h5>
                     </div>
                     <div className="card-body">
                         <h4 className="card-title">Por apenas U$ {this.props.flightOffer.price.total}</h4>
