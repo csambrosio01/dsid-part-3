cc41c67906980ae4736b19179364a7a22ea3c9ce
Caio Ambrosio <ambrosio.caio13@gmail.com>
2020-07-02 21:06:51 -0300

    Add prop id

--- src/components/toast/Toast.js
+++ src/components/toast/Toast.js
@@ -16,14 +16,14 @@ class Toast extends React.Component {
         setTimeout(() => {
             this.setState({showToast: false})
             setTimeout(() => {
-                document.getElementById('toast').hidden = true
+                document.getElementById(this.props.id).hidden = true
             }, 510)
         }, this.props.timeout ? this.props.timeout : 5000)
     }
 
     render() {
         return (
-            <div className={this.state.showToast ? 'fadeIn alert alert-danger' : 'fadeOut alert alert-danger'} id="toast">
+            <div className={this.state.showToast ? 'fadeIn alert alert-danger' : 'fadeOut alert alert-danger'} id={this.props.id}>
                 <h4 className="alert-heading">{this.props.header}</h4>
                 <p>{this.props.body}</p>
             </div>
